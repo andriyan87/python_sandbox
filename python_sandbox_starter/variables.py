5551baf1876a1a1067d468e7b735a68b227024b4
Andriyan <andriyanatanasov@gmail.com>
2025-02-08 00:56:02 +0200

    Documentation

---
+++ python_sandbox_starter/variables.py
@@ -0,0 +1,8 @@
+# A variable is a container for a value, which can be of various types
+
+"""
+VARIABLES RULES
+-- Variable names are case sensitive (name and NAME are different variables)
+-- Must start with a letter or an underscore
+-- Can have numbers but can not start with one
+"""
