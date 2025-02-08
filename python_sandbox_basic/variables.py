50390b8cb184b71643c25186b4b9319166ef5c16
Andriyan <andriyanatanasov@gmail.com>
2025-02-08 02:19:08 +0200

    casting

--- python_sandbox_basic/variables.py
+++ python_sandbox_basic/variables.py
@@ -6,3 +6,17 @@ VARIABLES RULES
 -- Must start with a letter or an underscore
 -- Can have numbers but can not start with one
 """
+
+
+# Multiple assignment
+x, y, name, is_true = (1, 2.34, 'Andriyan', True)
+
+# Basic math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(z), z)
